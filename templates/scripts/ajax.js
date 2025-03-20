cd186e4c2db71548a0c0144b80cd7122e38df133
POW4R <slemmerkevin@gmail.com>
2025-03-20 10:15:03 +0100

    feat: ajax.js fixing

---
+++ templates/scripts/ajax.js
@@ -0,0 +1,89 @@
+code="AAAAAAabc123";
+url="http://gamf.nhely.hu/ajax1/";
+
+async function read() {
+    let response = await fetch(url, {
+        method: 'post',
+        cache: 'no-cache',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: "code=" + code + "&op=read"
+    });
+    let data = await response.text();
+    data = JSON.parse(data);
+    let list = data.list;
+    let str = "<h1>Read</h1>";
+    str += "<p>Number of records: " + data.rowCount + "</p>";
+    str += "<p>Last max " + data.maxNum + " records:</p>";
+    str += "<table><tr><th>ID</th><th>Value</th><th>Height</th><th>Actions</th></tr>";
+    for (let i = 0; i < list.length; i++) {
+        str += `<tr>
+                    <td>${list[i].id}</td>
+                    <td contenteditable="true">${list[i].value}</td>
+                    <td contenteditable="true">${list[i].height}</td>
+                    <td><button onclick="updateData(${list[i].id})">Update</button>
+                        <button onclick="deleteData(${list[i].id})">Delete</button></td>
+                </tr>`;
+    }
+    str += "</table>";
+    document.getElementById("readDiv").innerHTML = str;
+}
+
+async function create() {
+    let value = document.getElementById("new-value").value;
+    let height = document.getElementById("new-height").value;
+    if (value.length > 0 && value.length <= 30 && height.length > 0) {
+        let response = await fetch(url, {
+            method: 'post',
+            cache: 'no-cache',
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            body: "code="+code+"&op=create&value="+value+"&height="+height
+        });
+        let data = await response.text();
+        document.getElementById("createResult").innerHTML = data > 0 ? "Create successful!" : "Create NOT successful!";
+        document.getElementById("new-value").value = "";
+        document.getElementById("new-height").value = "";
+        read();
+    } else {
+        document.getElementById("createResult").innerHTML = "Validation error!!";
+    }
+}
+
+async function updateData(id) {
+    let row = event.target.parentElement.parentElement;
+    let value = row.children[1].innerText;
+    let height = row.children[2].innerText;
+    if (value.length > 0 && value.length <= 30 && height.length > 0) {
+        let response = await fetch(url, {
+            method: 'post',
+            cache: 'no-cache',
+            headers: { 
+                'Content-Type': 'application/x-www-form-urlencoded' 
+            },
+            body: "code="+code+"&op=update&id="+id+"&value="+value+"height="+height
+        });
+        let data = await response.text();
+        document.getElementById("updateResult").innerHTML = data > 0 ? "Update successful!" : "Update NOT successful!";
+        read();
+    } else {
+        document.getElementById("updateResult").innerHTML = "Validation error!!";
+    }
+}
+
+async function deleteData(id) {
+    let response = await fetch(url, {
+        method: 'post',
+        cache: 'no-cache',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: `code=${code}&op=delete&id=${id}`
+    });
+    let data = await response.text();
+    document.getElementById("deleteResult").innerHTML = data > 0 ? "Delete successful!" : "Delete NOT successful!";
+    read();
+}
+
+window.onload = function () {
+    read();
+};
+
+const createButton = document.getElementById("createEntry");
+createButton.addEventListener("click", create);
